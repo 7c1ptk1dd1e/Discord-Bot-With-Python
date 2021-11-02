a41e59c053e0febcc2be7d31925cdf5eaa8a7ce1
Athon <93590599+7c1ptk1dd1e@users.noreply.github.com>
2021-11-02 13:33:18 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ from typing import Text
 import discord
 from discord import embeds
 from discord import channel
-from discord.colour import Color
+from discord.color import Color
 from discord.ext import commands
 
 TOKEN = 'Your Token'
@@ -24,9 +24,9 @@ async def YourCommandName(ctx):
   embed.set_footer(text="Your Text")
   await channel.send(content=None, embed=embed)
 
-
+#This is the name Of the Command 
   @bot.command()
-  async def Ole (ctx):
+  async def YourCommandName (ctx):
     print('Ready...')
     embed = discord.Embed(title="Your Text",url="Your URL", description="Your Text \n\n[Click Me](Your URL)", Color=0xf976)
     embed.set_thumbnail(url="Your URL")
